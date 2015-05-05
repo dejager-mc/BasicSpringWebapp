27ce626a0c2dda058bd420e247d9171043565406
dejager-mc <dejager.mc@gmail.com>
2015-05-05 15:39:24 +0200

    JMS Converting Message WERKT

---
+++ src/main/java/nl/simplewebapp/jms/ConvertJmsMessage.java
@@ -0,0 +1,12 @@
+package nl.simplewebapp.jms;
+
+/**
+ * Created by Machiel on 3-5-2015.
+ */
+public class ConvertJmsMessage {
+
+    public String convertMessage(String message) {
+        return message + " en extra text";
+    }
+
+}
