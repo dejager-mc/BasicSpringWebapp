d065e2cd562135eb306c693b613cb44731d28d57
dejager-mc <dejager.mc@gmail.com>
2015-05-05 22:46:21 +0200

    JMS aanpassingen voor demo

--- src/main/java/nl/demo/springjms/jms/ConvertJmsMessage.java
+++ src/main/java/nl/demo/springjms/jms/ConvertJmsMessage.java
@@ -1,4 +1,4 @@
-package nl.simplewebapp.jms;
+package nl.demo.springjms.jms;
 
 /**
  * Created by Machiel on 3-5-2015.
@@ -8,5 +8,4 @@ public class ConvertJmsMessage {
     public String convertMessage(String message) {
         return message + " en extra text";
     }
-
 }
